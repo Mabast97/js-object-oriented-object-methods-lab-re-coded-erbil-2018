240bf091bc65db544f4294b4476fff6a78de821e
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-30 19:56:33 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,9 @@
+function BoardMember(name, homeState, training){
+  this.name = name;
+  this.homeState = homeState;
+  this.training = training;
+  this.
+}
+
+let test = new BoardMember('me', 'hi', 'NY');
+test.veto();
