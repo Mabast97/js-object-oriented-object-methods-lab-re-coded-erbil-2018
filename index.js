d6937eac0088bc6b873ffada93a061caff400f45
Mabast Hashm Jamel <mabasthashm1@gmail.com>
2018-07-30 20:01:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ function BoardMember(name, homeState, training){
   this.name = name;
   this.homeState = homeState;
   this.training = training;
+  this.veto = function(){console.log("No, I must disagree")};
 }
 
 let polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law");
-polishedBoarderMember.veto()='No, I '
+
